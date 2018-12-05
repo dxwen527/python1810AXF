59c893bb094f5fa58c20fe1667b8e9e75cadeae5
sevenasdf <1924167890@qq.com>
2018-12-05 20:18:06 +0800

    项目基本搭建完成

--- APP/views.py
+++ APP/views.py
@@ -2,5 +2,33 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse('爱鲜蜂首页')
+from APP.models import Wheel, Nav, Mustbuy
+
+
+def home(request):
+    # 获取轮播图数据
+    wheels = Wheel.objects.all()
+
+    # 导航数据
+    navs = Nav.objects.all()
+
+    # 每日必购
+    mustbuys = Mustbuy.objects.all()
+    data = {
+        'wheels':wheels,
+        'navs':navs,
+        'mustbuys':mustbuys,
+    }
+    return render(request,'home/home.html',context=data)
+
+
+def market(request):
+    return render(request,'market/market.html')
+
+
+def cart(request):
+    return render(request,'cart/cart.html')
+
+
+def mine(request):
+    return render(request,'mine/mine.html')
